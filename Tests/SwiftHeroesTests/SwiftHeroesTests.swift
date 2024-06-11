05c1f91df7fe65b6407dd8b3c7d7a0a31a547f04
Shai Mishali <freak4pc@gmail.com>
2024-06-11 13:50:59 -0700

    ST

--- Tests/SwiftHeroesTests/SwiftHeroesTests.swift
+++ Tests/SwiftHeroesTests/SwiftHeroesTests.swift
@@ -2,15 +2,14 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
-import XCTest
+import Testing
 import MacroTesting
 
 #if canImport(MacrosTalkMacros)
 import MacrosTalkMacros
 #endif
 
-final class MacrosTalkTests: XCTestCase {
-    func testCased() throws {
-        
-    }
+@Test("Generates properties and boolean for each case")
+func testCased() async throws {
+
 }
