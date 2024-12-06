4d172ed0db66bcd43260108376cf868be2ef9b63
Shai Mishali <freak4pc@gmail.com>
2024-12-06 21:57:54 +0200

    swift-testing

--- Tests/SwiftHeroesTests/SwiftHeroesTests.swift
+++ Tests/SwiftHeroesTests/SwiftHeroesTests.swift
@@ -2,14 +2,16 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
-import XCTest
+import Testing
 import MacroTesting
 
 #if canImport(MacrosTalkMacros)
 import MacrosTalkMacros
 #endif
 
-class CasedTests: XCTestCase {
+@Suite
+struct CasedTests {
+    @Test("@Cased on enum")
     func testCased() async throws {
 
     }
